0472c717a71708563d09bbc5c667e3430d491eca
Nandini Gangwar <nandinis898@gmail.com>
2026-03-14 23:16:44 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ const port = process.env.PORT || 5000;
 const SECRET_KEY = process.env.SECRET_KEY;
 const bcrypt = require('bcrypt');
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
